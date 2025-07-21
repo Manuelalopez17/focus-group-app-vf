c19503c932f9ec7bb3fd6d05b78427fd7cf2cdfe
Manuela Lopez Echeverry <lopezecheverrymanuela@gmail.com>
2025-07-20 21:55:55 -0400

    Actualización del componente Participante con conexión Supabase

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App.jsx';
+import Participante from './assets/components/Participante.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/participante" element={<Participante />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+
