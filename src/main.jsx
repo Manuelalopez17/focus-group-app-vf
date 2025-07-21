a38a18b61783ca2617ae5078ac121312ba9034d6
Manuela Lopez Echeverry <lopezecheverrymanuela@gmail.com>
2025-07-20 22:02:18 -0400

    Corrige main.jsx para restaurar el dashboard como vista principal

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App.jsx';
-import Participante from './assets/components/Participante.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/participante" element={<Participante />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
