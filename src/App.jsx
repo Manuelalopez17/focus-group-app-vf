fb1447f2786fc0ebb84a19c1e2c03d2e39f0d7ce
Manuela Lopez Echeverry <lopezecheverrymanuela@gmail.com>
2025-07-29 17:39:27 -0400

    Integrar LandingPage en ruta raíz

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
+// src/App.jsx
 import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Presentacion from './Pages/Presentacion'
-import Home from './Pages/Home'
-import Participante from './Pages/Participante'    // <-- ahora existe aquí
+import LandingPage   from './Pages/LandingPage'
+import Presentacion  from './Pages/Presentacion'
+import Home          from './Pages/Home'
+import Participante  from './Pages/Participante'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/presentacion" replace />} />
+        {/* 1. Pantalla de bienvenida */}
+        <Route path="/" element={<LandingPage />} />
+
+        {/* 2. Rutas ya existentes */}
         <Route path="/presentacion" element={<Presentacion />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/participante" element={<Participante />} />
-        <Route path="*" element={<Navigate to="/presentacion" replace />} />
+        <Route path="/home"          element={<Home />} />
+        <Route path="/participante"  element={<Participante />} />
+
+        {/* 3. Fallback: vuelve al landing */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   )
